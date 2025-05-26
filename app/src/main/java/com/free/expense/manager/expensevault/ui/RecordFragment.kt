ff092064d0878e8f32c9fa34668bc2ffae7de8b4
Rifat Hossain <mdrifathossain472@gmail.com>
2025-05-27 01:05:59 +0600

    initial commit

--- app/src/main/java/com/free/expense/manager/expensevault/ui/RecordFragment.kt
+++ app/src/main/java/com/free/expense/manager/expensevault/ui/RecordFragment.kt
@@ -1,5 +1,6 @@
-package com.free.expense.manager.expensevault
+package com.free.expense.manager.expensevault.ui
 
+import com.free.expense.manager.expensevault.base.BaseFragment
 import com.free.expense.manager.expensevault.databinding.FragmentRecordBinding
 
 
@@ -11,5 +12,4 @@ class RecordFragment : BaseFragment<FragmentRecordBinding>(FragmentRecordBinding
     override fun allObserver() {
 
     }
-
 }
